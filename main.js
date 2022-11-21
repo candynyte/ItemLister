343e19909e553ea152bb34bb465795dd3e725b67
candynyte <115418932+candynyte@users.noreply.github.com>
2022-11-21 10:05:24 +0300

    final commit

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 const form = document.querySelector("#addForm");
+const main = document.querySelector("#main");
 let itemList = document.querySelector("#items");
 let filterBar = document.querySelector("#filter");
 
 // Form submit event
-
 form.addEventListener("submit", addItem);
-// console.log(itemList);
 
 // Remove item event
-
 itemList.addEventListener("click",removeItem);
-filterBar.addEventListener("keyup",filterItems)
-// itemList.addEventListener("click",tagForDone);
 
+// Filtering item event
+filterBar.addEventListener("keyup",filterItems)
 
 function hide(element){
     element.style = "display:none;";
@@ -21,25 +19,26 @@ function hide(element){
 
 function addItem(e)
 {   
-    // preventing the events default behaviour (submit).
+    // preventing the events default behaviour(submit).
     e.preventDefault();
+
     var formInput = document.querySelector("#formInput");
 
     // Append items to itemList
     
+    // Checking if item input is empty. 
     if (formInput.value === "") 
     {
-        // Warning if input is empty.
+        // Throwing a warning if input is empty.
 
         // Dont forget to add smoothness !!
-
-        const main = document.querySelector("#main");
-
-        // creating warning element.
+        
+        // Creating warning element.
         let warning = document.createElement("h6");
         warning.id = "warning";
         warning.className = "text-danger";
         warning.innerHTML = "Please enter item name.";
+        // Making warning disappear after a certain miliseconds
         setTimeout(function(){hide(warning)}, 1000);
         main.insertBefore(warning,form);
 
@@ -47,32 +46,30 @@ function addItem(e)
     }
     else
     {
-
-        // creating li element
+        // Creating li element
         let li = document.createElement("li");
         li.className = "list-group-item";
-
-
-        // alternative way.
-
-        // newItem.innerHTML = (formInput.value + " <button class='btn btn-danger btn-sm float-end'>X</button>"); 
-        // itemList.insertBefore(newItem,itemList.children[-1]);
-
         li.appendChild(document.createTextNode(formInput.value));
         itemList.appendChild(li);
 
-        // Creating the button for removing items
+        // Creating the button for removing items.
         let deleteBtn = document.createElement("button");
         deleteBtn.className = "btn btn-danger btn-sm float-end delete";
         deleteBtn.appendChild(document.createTextNode("X"));
         li.appendChild(deleteBtn);
 
+        // Alternate way.
+
+        // newItem.innerHTML = (formInput.value + " <button class='btn btn-danger btn-sm float-end'>X</button>"); 
+        // itemList.insertBefore(newItem,itemList.children[-1]);
+
         // Done Btn
         // let doneBtn = document.createElement("button"); 
-        // doneBtn.className = "btn btn-success btn-sm float-end done mr-4"
+        // doneBtn.className = "btn btn-success btn-sm float-end done"
         // doneBtn.appendChild(document.createTextNode("D"));
         // li.appendChild(doneBtn);
     }
+    // Reset the input value.
     formInput.value = "";
    
 }
@@ -80,43 +77,36 @@ function addItem(e)
 
 function removeItem(e){
 
-    // target gives us the location of event and if location's classlist contains "delete" that means we are clicking the button. 
+    // e.target gives us the location of event and if location's classlist contains "delete" that means we are clicking the button! 
 
     if(e.target.classList.contains("delete"))
     { 
         itemList.removeChild(e.target.parentElement);
-
-        // if(confirm("Are you sure ? ")){
-        //     itemList.removeChild(e.target.parentElement);
-        // }
-        // console.log(1)
     }
 }
 
 
 function filterItems(e){
-    let filterBarInput = filterBar.value.toLowerCase();
+    let filterBarInput = e.target.value.toLowerCase();
     let items = document.getElementsByTagName("li");
     
-    for(i=0; i<items.length; i++){
-
-        textofItem = items[i].textContent || items[i].innerText;
-        if(textofItem.toLowerCase().indexOf(filterBarInput) > -1){
-            items[i].style.display="";
+    Array.from(items).forEach(function(item){
+        let textofItem = item.firstChild.textContent;
+        if(textofItem.toLowerCase().indexOf(filterBarInput) != -1){
+            item.style.display = "block";
         }else{
-            items[i].style.display="none";
-        }
+            item.style.display="none";
+        }    });
 
-    }
+    // Alternate way
+
+    // for(i=0; i<items.length; i++){
+    //     textofItem = items[i].textContent || items[i].innerText;
+    //     if(textofItem.toLowerCase().indexOf(filterBarInput) > -1){
+    //         items[i].style.display="block";
+    //     }else{
+    //         items[i].style.display="none";
+    //     }
+
+    // }
 }
-
-
-
-
-// function tagForDone(e){
-//     if(e.target.classList.contains("done"))
-//     { 
-//         e.target.parentElement.style.textDecoration = "line-through"        
-//     }
-
-// }
